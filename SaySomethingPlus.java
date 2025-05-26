7d914306961f3093e410baa8af7713e00594f2cc
Pascal <jpaccjpoff@gmail.com>
2025-05-26 19:35:56 -0400

    Add SaySomethingPlus.java
    Main program

---
+++ SaySomethingPlus.java
@@ -0,0 +1,40 @@
+// Program: SaySomething+
+// Description: An improvement upon SaySomething, a cowsay-inspired program.
+
+import java.util.Scanner; // Imports Scanner utility.
+import utils.*;
+
+public class SaySomethingPlus {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.println(Formatting.ITALIC + "Say" + Formatting.RESET + Formatting.BOLD_ITALIC + "Something" + Formatting.YELLOW + "+" + Formatting.RESET); 
+        System.out.println("(0) 2025 CommonPolarity. No rights reserved.");
+        System.out.println("This program is licensed under the MIT License.");
+
+        while (true) {
+            System.out.print("What do you want me to say?: ");
+            String say = kbd.nextLine();
+
+            if (say.equals("!exit")) {
+                Exit exec = new Exit();
+                boolean shouldExit = exec.leave(kbd);
+                if (shouldExit) {
+                    break;
+                }
+            } else if (say.equals("!btform")) {
+                BTFormQuestion ask = new BTFormQuestion();
+                boolean execMode = ask.formExec(kbd);
+                System.out.println("BTForm mode enabled. To exit the program, you must type !exitprog.");
+                System.out.println("To only exit BTForm mode, just type !exit.");
+                while (execMode) {
+                    BTFormImplementation addMore = new BTFormImplementation();
+                    execMode = addMore.startImplementation(kbd);
+                }
+            } else {
+                BoxConstruction msgBox = new BoxConstruction();
+                msgBox.construct(say);
+            }
+        }
+        kbd.close();
+    }
+}
