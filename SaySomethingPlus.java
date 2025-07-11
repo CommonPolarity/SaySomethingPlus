d3833dd12f689c6e68e47ecbdc8da1a7ee8cd321
Pascal <jpaccjpoff@gmail.com>
2025-07-10 21:27:07 -0400

    Switched from if-then to switch statements (v1.1)
    First major update. Still haven't fixed the one issue with BTForm mode (/_\)

--- SaySomethingPlus.java
+++ SaySomethingPlus.java
@@ -7,7 +7,7 @@ import utils.*;
 public class SaySomethingPlus {
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
-        System.out.println(Formatting.ITALIC + "Say" + Formatting.RESET + Formatting.BOLD_ITALIC + "Something" + Formatting.YELLOW + "+" + Formatting.RESET); 
+        System.out.println(Formatting.ITALIC + "Say" + Formatting.RESET + Formatting.BOLD_ITALIC + "Something" + Formatting.YELLOW + "+" + Formatting.RESET + "v1.1"); 
         System.out.println("(0) 2025 CommonPolarity. No rights reserved.");
         System.out.println("Need help doing something? Go & type !help.");
         System.out.println("This program is licensed under the MIT License.");
@@ -16,29 +16,34 @@ public class SaySomethingPlus {
             System.out.print("What do you want me to say?: ");
             String say = kbd.nextLine();
 
-            if(say.equals("!exit")) {
-                Exit exec = new Exit();
-                boolean shouldExit = exec.leave(kbd);
-                if (shouldExit) {
-                    break;
-                }
-            } else if(say.equals("!btform")) {
-                BTFormQuestion ask = new BTFormQuestion();
-                boolean execMode = ask.formExec(kbd);
-                System.out.println("BTForm mode enabled. To exit the program, you must type !exitprog.");
-                System.out.println("To only exit BTForm mode, just type !exit.");
-                while (execMode) {
-                    BTFormImplementation addMore = new BTFormImplementation();
-                    execMode = addMore.startImplementation(kbd);
-                }
-            } else if(say.equals("!help")) {
-                Help display = new Help();
-                display.help(say);
-            } else {
-                BoxConstruction msgBox = new BoxConstruction();
-                msgBox.construct(say);
+            switch(say) {
+                case "!exit":
+                    Exit exec = new Exit();
+                    boolean shouldExit = exec.leave(kbd);
+                    if (shouldExit) {
+                        break;
+                    }
+                    continue;
+                case "!btform":
+                    BTFormQuestion ask = new BTFormQuestion();
+                    boolean execMode = ask.formExec(kbd);
+                    System.out.println("BTForm mode enabled. To exit the program, you must type !exitprog.");
+                    System.out.println("To only exit BTForm mode, just type !exit.");
+                    while (execMode) {
+                        BTFormImplementation addMore = new BTFormImplementation();
+                        execMode = addMore.startImplementation(kbd);
+                    }
+                    return;
+                case "!help":
+                    Help display = new Help();
+                    display.help(say);
+                    return;
+                default:
+                    BoxConstruction msgBox = new BoxConstruction();
+                    msgBox.construct(say);
+                    return;
             }
+            kbd.close();
         }
-        kbd.close();
     }
 }
